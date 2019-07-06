b05fa281bccbc5ee07113571adac74303d220858
hoang <minhhoangho99@gmail.com>
2019-07-06 10:40:46 +0700

    nothing

--- app/Dashboard/Controllers/DashboardController.js
+++ app/Dashboard/Controllers/DashboardController.js
@@ -1,18 +1,8 @@
 const translate = require('@k3rn31p4nic/google-translate-api');
 import solve from '../../../infrastructure/Helpers/Wolframalpha'
 class DashboardController {
-  async index(req, res) {
-    let question = 'Hằng số PI là bao nhiêu?'
-
-      question = await translate(question, {from:'vi', to:'en'});
-      
-      const result = await solve(question.text)
-      // console.log(result);
-      
-      const answer = await translate(result, {from:'en', to:'vi'});
-      console.log(answer.text);
-      
-      return res.render('app/client/dashboard/index');
+   index(req, res) {
+    return res.render('app/client/dashboard/index');
   }
 }
 
